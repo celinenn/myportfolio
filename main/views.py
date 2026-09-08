7706814e39cd7d075ea3c2a1cb21a28926c48d70
celinenn <celinenafisas@gmail.com>
2026-09-09 01:59:03 +0700

    Finished Tutorial 2

---
+++ main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Celine",
+        "npm": "2506590201",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "I am a self-motivated and creative individual with a great passion "
+            "for the IT industry. I am currently in my first year of studying "
+            "Computer Science at the University of Indonesia. I am organized "
+            "in completing my tasks, allowing me to finish them in time, and "
+            "able to work in a team."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Celine",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
